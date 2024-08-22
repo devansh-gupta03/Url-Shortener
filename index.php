9b2a243ca0f571b4f5a9d261ab84084ff36e29af
devansh-gupta03 <142433214+devansh-gupta03@users.noreply.github.com>
2024-08-22 18:42:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -53,11 +53,9 @@ echo "No short code provided!";
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>URL Shortener</title>
+   <title>URL Shortener</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body {
